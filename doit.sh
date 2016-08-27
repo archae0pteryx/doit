cb377733916dc5708b83a2e23cc933c25d242c8a
archae0pteryx <jesus.christ@openmailbox.org>
2016-08-27 09:47:55 -0700

    added autossh

--- doit.sh
+++ doit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-d_keyfile='0000000'
+d_keyfile='~/Code/Repos/kittens/0000000'
 d_name='monkies'
 d_image='ubuntu-16-04-x64'
 d_size='512mb'
@@ -7,6 +7,7 @@ d_reg='sfo1'
 d_key='fb:86:91:f8:a8:d2:76:39:dd:bb:61:3d:a4:13:97:fa'
 
 doctl compute droplet create $d_name --region $d_reg --image $d_image	--size $d_size --ssh-keys $d_key
+echo ""
 echo "Give the monkies a min to squabble..."
 sleep 3
 echo "..."
@@ -18,3 +19,7 @@ sleep 3
 echo "POW!"
 ipv4=$(doctl compute droplet list | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 echo $ipv4
+echo ""
+echo "trying ssh"
+sleep 5
+ssh -vi d_keyfile root@$ipv4
