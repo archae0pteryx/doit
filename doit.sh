9042886c6cf39ee7359d13b5899633bd0927a47d
archae0pteryx <jesus.christ@openmailbox.org>
2016-08-26 20:14:10 -0700

    git init

---
+++ doit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+d_keyfile='0000000'
+d_name='monkies'
+d_image='ubuntu-16-04-x64'
+d_size='512mb'
+d_reg='sfo1'
+d_key='fb:86:91:f8:a8:d2:76:39:dd:bb:61:3d:a4:13:97:fa'
+
+doctl compute droplet create $d_name --region $d_reg --image $d_image	--size $d_size --ssh-keys $d_key
+echo "Give the monkies a min to squabble..."
+sleep 3
+echo "..."
+sleep 4
+echo "0_o"
+sleep 7
+echo "Wait for it"
+sleep 3
+echo "POW!"
+ipv4=$(doctl compute droplet list | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+echo $ipv4
