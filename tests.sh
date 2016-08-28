9e6eba498487179d2c688baa51a6228f87deece9
archae0pteryx <jesus.christ@openmailbox.org>
2016-08-27 21:54:01 -0700

    Just do it.

---
+++ tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+php libapache2-mod-php php-mcrypt php-mysql python-letsencrypt-apache
+
+
+/etc/apache2/sites-enabled/000-default.conf
+ #ServerName www.example.com/ServerName <whatever.whatever>
