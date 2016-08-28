9e0e1b0ff748d61515f23973468359edc6f5976d
archae0pteryx <jesus.christ@openmailbox.org>
2016-08-27 21:15:44 -0700

    fixed dumb string

--- doit.sh
+++ doit.sh
@@ -5,7 +5,8 @@ d_image='ubuntu-16-04-x64'
 d_size='512mb'
 d_reg='sfo1'
 d_key='fb:86:91:f8:a8:d2:76:39:dd:bb:61:3d:a4:13:97:fa'
-
+clear
+echo ""
 doctl compute droplet create $d_name --region $d_reg --image $d_image	--size $d_size --ssh-keys $d_key
 echo ""
 echo "Did you see that bitch take a camera phone photo of me!?"
@@ -19,13 +20,14 @@ sleep 3
 echo "Wait for it"
 sleep 3
 echo "Wait for it"
-echo 2
+sleep 1
 echo "...waaait for it..."
 sleep 7
 echo "Maul IT!!!!"
 sleep 2
 clear
-"PLADOW!"
+echo ""
+"PLADOW\!"
 sleep 2
 echo "Delicious fat bitches IP:"
 ipv4=$(doctl compute droplet list | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
