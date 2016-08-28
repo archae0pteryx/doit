8ef5d762153b81dfa57dc1547c26e9b1ab136c03
archae0pteryx <jesus.christ@openmailbox.org>
2016-08-28 10:23:43 -0700

    politeness added

--- doit.sh
+++ doit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 d_keyfile="0000000"
-d_name='default'
+d_name='maria'
 d_image='ubuntu-16-04-x64'
 d_size='512mb'
 d_reg='sfo1'
@@ -8,6 +8,7 @@ d_key='fb:86:91:f8:a8:d2:76:39:dd:bb:61:3d:a4:13:97:fa'
 clear
 echo ""
 doctl compute droplet create $d_name --region $d_reg --image $d_image	--size $d_size --ssh-keys $d_key
+echo "Please wait..."
 sleep 13
 echo "Droplets IP:"
 ipv4=$(doctl compute droplet list | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
